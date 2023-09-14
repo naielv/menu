bbe291f283d54821a31568cdd880e835fb478cc9
naielv <109038805+naielv@users.noreply.github.com>
2023-09-14 13:50:16 +0200

    Create download.py

---
+++ download.py
@@ -0,0 +1,11 @@
+import requests
+from pdf2image import convert_from_path
+
+def pdf_to_png(url, filename)
+    res = requests.request("GET", url).content
+    with open("tmp.pdf", "wb") as f:
+        f.write(res)
+    images = convert_from_path('tmp.pdf')
+    images[0].save(filename, 'JPEG')
+
+pdf_to_png("https://menuak.ausolan.com/documentos/AUZOLAGUN/CAS-AUZO-IS-priv.pdf", "menu-ausolan-generico.jpg")
